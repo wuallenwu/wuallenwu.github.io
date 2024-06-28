e7382850590edb825610e2d267a4f18f1953ad9f
wuallenwu <allenwu.12@outlook.com>
2024-06-27 17:58:57 -0700

    webiste stuff and registration stuff

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('includes/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        });
-});
